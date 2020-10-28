404026a3a27d88de288bc4f1f29189a4e8b0c48a
xexe <francesccoll@paucasesnovescifp.cat>
2020-10-28 20:53:56 +0100

    Challeng, Demanar una data anterior i dir qunts de dies han passat

---
+++ EjerciciosC#/DateTimeChallenge/DateTimeChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+namespace DateTimeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine("Introduce una fecha: ");
+            String data = Console.ReadLine();
+
+            DateTime dataAnterior = DateTime.ParseExact(data, "dd/MM/yyyy", null);//Entiendo que tengo que poner null por la hora pero no entiendo que no funcione sin el null.
+            TimeSpan diasPasats = DateTime.Now.Subtract(dataAnterior);
+
+            Console.WriteLine("Han passat: " + diasPasats.Days + " dies");
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Introduce una hora: ");
+            String hora = Console.ReadLine();
+
+
+        }
+    }
+}
