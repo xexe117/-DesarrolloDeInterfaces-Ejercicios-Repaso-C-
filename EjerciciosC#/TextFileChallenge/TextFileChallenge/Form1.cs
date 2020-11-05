d8457ae44dbc9100b9b3f11c0b3b60b8869293cc
xexe <francesccoll@paucasesnovescifp.cat>
2020-11-05 20:33:34 +0100

    Challenge, text file 2p parte Nombre Apellido Edad Vivo/muerto en un file(.csv) desordenador

--- EjerciciosC#/TextFileChallenge/TextFileChallenge/Form1.cs
+++ EjerciciosC#/TextFileChallenge/TextFileChallenge/Form1.cs
@@ -15,6 +15,11 @@ namespace TextFileChallenge
     {
         BindingList<UserModel> users = new BindingList<UserModel>();
 
+        int OrdenNombre = 0;
+        int OrdenApellido = 0;
+        int Edad = 0;
+        int VM= 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,12 +33,34 @@ namespace TextFileChallenge
         {
             String[] lines = File.ReadAllLines("UserList.csv");
 
+            string[] headers = lines[0].Split(',');
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                if (headers[1] == "FirstName")
+                {
+                    OrdenNombre = i;
+                }
+                else if (headers[1] == "LastName")
+                {
+                    OrdenApellido = i;
+                }
+                if (headers[1] == "Age")
+                {
+                    Edad = i;
+                }
+                if (headers[1] == "IsAlive")
+                {
+                    VM = i;
+                }
+            }
+        
             for (int i = 1; i < lines.Length; i++)
             {
                 string[] columns = lines[i].Split(',');
                 bool isAlive = false;
 
-                if (columns[3] == "1")
+                if (columns[VM] == "1")
                 {
                     isAlive = true;
                 }
@@ -41,9 +68,9 @@ namespace TextFileChallenge
 
                 users.Add(new UserModel
                 {
-                    FirstName = columns[0],
-                    LastName = columns[1],
-                    Age = int.Parse(columns[2]),
+                    FirstName = columns[OrdenNombre],
+                    LastName = columns[OrdenApellido],
+                    Age = int.Parse(columns[Edad]),
                     IsAlive = isAlive
 
                 });
@@ -75,22 +102,78 @@ namespace TextFileChallenge
 
         private void saveList_Click_1(object sender, EventArgs e)
         {
-                List<String> lines = new List<string>
-            {
-                "FirstName,LastName,Age,IsAlive"
-            };
+            List<string> lines = new List<string>;//No entiendo porque aqui me da error
 
-                foreach (UserModel user in users)
+            string line = "";
+
+            //{
+            // "FirstName,LastName,Age,IsAlive"
+            //};
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (OrdenNombre == i)
+                {
+                    line += "FirstName";
+                }
+                else if (OrdenApellido == i)
+                {
+                    line = "LastName";
+                }
+                else if (Edad == i)
+                {
+                    line = "Age";
+                }
+                else if (VM == i)
+                {
+                    line = "IsAlive";
+                }
+
+                line += ",";
+            }
+
+            line = line.Substring(0, line.Length - 1);
+
+            lines.Add(line);
+
+            foreach (UserModel user in users)
                 {
                     int isAliveValor = 0;
+                    line = "";
 
                     if (user.IsAlive == true)
                     {
                         isAliveValor = 1;
                     }
+                    
+                    for (int i = 0; i < 4; i++)
+                        {
+                        if (OrdenNombre == i)
+                        {
+                            line += user.FirstName;
+                        }
+                        else if (OrdenApellido == i)
+                        {
+                            line += user.LastName;
+                        }
+                        else if (Edad == i)
+                        {
+                            line += user.Age;
+                        }
+                        else if (VM == i)
+                        {
+                            line += user.IsAlive;
+                        }
 
-                    lines.Add($"{ user.FirstName },{ user.LastName },{ user.Age },{ isAliveValor }");
+                    line += ",";
                 }
+
+                line = line.Substring(0, line.Length - 1);
+
+                lines.Add(line);
+
+                //lines.Add($"{ user.FirstName },{ user.LastName },{ user.Age },{ isAliveValor }");
+            }
 
                 File.WriteAllLines("UserList.csv", lines);
                 MessageBox.Show("Save complete");
